60348cbdc509bc0fb12f4311252fb46016cb3290
8-TECH <153704930+8-TECH@users.noreply.github.com>
2024-09-24 06:59:18 +0300

    Add files via upload

---
+++ faith/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class Faith(models.Model):
+    religion = models.CharField(max_length=200)
+    short_form = models.CharField(max_length=100, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    phone = models.CharField(max_length=9, default='113631232')
+    officials_name = models.CharField(max_length=100, default='8Tech')
+    link = models.URLField(max_length=1000, blank=True, null=True)
+    linkname = models.CharField(max_length=100, default='Link')
+
+    def __str__(self):
+        return self.religion
