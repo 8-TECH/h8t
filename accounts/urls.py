201f7d681890b6a5acef2a359b5bffd2d2a29206
8-TECH <153704930+8-TECH@users.noreply.github.com>
2024-09-24 06:56:52 +0300

    Add files via upload

---
+++ accounts/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from . import views
+
+app_name = "accounts"
+
+urlpatterns = [
+    path('register',views.register, name='register'),
+    path('login',views.login, name='login'),
+    path('logout',views.logout, name='logout'),
+    path('password_reset/', views.password_reset_request, name='password_reset'),
+    path('reset/<uidb64>/<token>/', views.password_reset_confirm, name='password_reset_confirm'),
+    path('password_reset/done/', views.password_reset_done, name='password_reset_done'),  
+    path('reset/done/', views.password_reset_complete, name='password_reset_complete'), 
+]
